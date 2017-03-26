dec441e3943b8b2dd12d363826a07da4d29eefc4
liuxingrichu <liuxingrichu@163.com>
2017-03-26 22:01:38 +0800

    【功能】简单远程RabbitMQ服务

---
+++ rpc_remote/remote_consumer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import pika
+
+credentials = pika.PlainCredentials('test', '123456')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    '192.168.31.101', 5672, '/', credentials))
+channel = connection.channel()
+
+# You may ask why we declare the queue again ‒
+# we have already declared it in our previous code.
+# We could avoid that if we were sure that the queue already exists.
+# For example if send.py program
+# was run before. But we're not yet sure which program to run first.
+# In such cases it's a good
+# practice to repeat declaring the queue in both programs.
+channel.queue_declare(queue='balance')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+channel.basic_consume(callback,
+                      queue='balance',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
