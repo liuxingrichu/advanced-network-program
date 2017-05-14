cf4fe1fb4e9bb4d5cd1dd0de865858ecaa3c183c
liuxingrichu <liuxingrichu@163.com>
2017-05-14 23:01:31 +0800

    【功能】学员管理系统 【已实现】表结构、代码主结构 【待完成】学员视图和教师视图的功能代码

---
+++ student_manage_system/modules/student_center.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from .models import Student
+
+class StudentCenter(object):
+    def __init__(self):
+        pass
+
+    def handler(self):
+        while True:
+            pass
+
+    def authentication(self):
+        pass
+
+    def up_homework(self):
+        pass
+
+    def check_score(self):
+        pass
+
+    def rank(self):
+        pass
---
+++ student_manage_system/modules/teacher_center.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+class TeacherCenter(object):
+    def __init__(self):
+        pass
+
+    def create_class(self):
+        pass
+
+    def change_record(self):
+        pass
+
+    def change_score(self):
+        pass
