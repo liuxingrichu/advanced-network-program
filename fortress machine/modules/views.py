859e85b8b617c02bcbaf107e64c16419ad7e5658
liuxingrichu <liuxingrichu@163.com>
2017-07-24 23:48:18 +0800

    【功能】堡垒机 V1.5 【新增】完善添加堡垒机用户的组关联和主机关联 【实现1】表结构、添加主机、添加远程用户、添加堡垒机用户（未关联）、添加组（未关联） 【实现2】添加绑定主机（bind_host、bindhost_m2m_hostgroup、user_m2m_bindhost） 【实现2】完善添加堡垒机用户的组关联和主机关联

--- fortress machine/modules/views.py
+++ fortress machine/modules/views.py
@@ -6,6 +6,7 @@ from modules.utils import print_err
 from modules.utils import yaml_parser
 from modules.db_conn import session
 from modules.db_conn import engine
+from modules import common_filters
 
 
 def syncdb(argvs):
@@ -78,17 +79,17 @@ def create_fortressusers(argvs):
             print(key, val)
             obj = model_v2.FortressUser(username=key,
                                         password=val.get('password'))
-            # if val.get('groups'):
-            #     groups = session.query(models.Group).filter(
-            #         model_v2.Group.name.in_(val.get('groups'))).all()
-            #     if not groups:
-            #         print_err("none of [%s] exist in group table." % val.get(
-            #             'groups'), quit=True)
-            #     obj.groups = groups
-            # if val.get('bind_hosts'):
-            #     bind_hosts = common_filters.bind_hosts_filter(val)
-            #     obj.bind_hosts = bind_hosts
-            # print(obj)
+            if val.get('groups'):
+                groups = session.query(model_v2.HostGroup).filter(
+                    model_v2.HostGroup.name.in_(val.get('groups'))).all()
+                if not groups:
+                    print_err("none of [%s] exist in group table." % val.get(
+                        'groups'), quit=True)
+                obj.host_groups = groups
+            if val.get('bind_hosts'):
+                bind_hosts = common_filters.bind_hosts_filter(val)
+                obj.bind_hosts = bind_hosts
+            print(obj)
             session.add(obj)
         session.commit()
 
@@ -114,9 +115,9 @@ def create_groups(argvs):
             #     bind_hosts = common_filters.bind_hosts_filter(val)
             #     obj.bind_hosts = bind_hosts
             #
-            # if val.get('user_profiles'):
-            #     user_profiles = common_filters.user_profiles_filter(val)
-            #     obj.user_profiles = user_profiles
+            # if val.get('fortress_user'):
+            #     fortress_users = common_filters.fortress_user_filter(val)
+            #     obj.fortress_users = fortress_users
             session.add(obj)
         session.commit()
 
@@ -179,3 +180,101 @@ def create_bindhosts(argvs):
                     bindhost_obj.fortress_users = fortressuser_objs
                     # print(bindhost_obj)
         session.commit()
+
+
+# def auth():
+#     '''
+#     do the user login authentication
+#     :return:
+#     '''
+#     count = 0
+#     while count < 3:
+#         username = input("\033[32;1mUsername:\033[0m").strip()
+#         if len(username) == 0: continue
+#         password = input("\033[32;1mPassword:\033[0m").strip()
+#         if len(password) == 0: continue
+#         user_obj = session.query(models.UserProfile).filter(
+#             models.UserProfile.username == username,
+#             models.UserProfile.password == password).first()
+#         if user_obj:
+#             return user_obj
+#         else:
+#             print("wrong username or password, you have %s more chances." % (
+#             3 - count - 1))
+#             count += 1
+#     else:
+#         print_err("too many attempts.")
+#
+#
+# def welcome_msg(user):
+#     WELCOME_MSG = '''\033[32;1m
+#     ------------- Welcome [%s] login LittleFinger -------------
+#     \033[0m''' % user.username
+#     print(WELCOME_MSG)
+#
+#
+# def start_session(argvs):
+#     print('going to start sesssion ')
+#     user = auth()
+#     if user:
+#         welcome_msg(user)
+#         print(user.bind_hosts)
+#         print(user.groups)
+#         exit_flag = False
+#         while not exit_flag:
+#             if user.bind_hosts:
+#                 print('\033[32;1mz.\tungroupped hosts (%s)\033[0m' % len(
+#                     user.bind_hosts))
+#             for index, group in enumerate(user.groups):
+#                 print('\033[32;1m%s.\t%s (%s)\033[0m' % (
+#                 index, group.name, len(group.bind_hosts)))
+#
+#             choice = input("[%s]:" % user.username).strip()
+#             if len(choice) == 0: continue
+#             if choice == 'z':
+#                 print("------ Group: ungroupped hosts ------")
+#                 for index, bind_host in enumerate(user.bind_hosts):
+#                     print("  %s.\t%s@%s(%s)" % (index,
+#                                                 bind_host.remoteuser.username,
+#                                                 bind_host.host.hostname,
+#                                                 bind_host.host.ip_addr,
+#                                                 ))
+#                 print("----------- END -----------")
+#             elif choice.isdigit():
+#                 choice = int(choice)
+#                 if choice < len(user.groups):
+#                     print("------ Group: %s ------" % user.groups[choice].name)
+#                     for index, bind_host in enumerate(
+#                             user.groups[choice].bind_hosts):
+#                         print("  %s.\t%s@%s(%s)" % (index,
+#                                                     bind_host.remoteuser.username,
+#                                                     bind_host.host.hostname,
+#                                                     bind_host.host.ip_addr,
+#                                                     ))
+#                     print("----------- END -----------")
+#
+#                     # host selection
+#                     while not exit_flag:
+#                         user_option = input(
+#                             "[(b)back, (q)quit, select host to login]:").strip()
+#                         if len(user_option) == 0: continue
+#                         if user_option == 'b': break
+#                         if user_option == 'q':
+#                             exit_flag = True
+#                         if user_option.isdigit():
+#                             user_option = int(user_option)
+#                             if user_option < len(
+#                                     user.groups[choice].bind_hosts):
+#                                 print('host:', user.groups[choice].bind_hosts[
+#                                     user_option])
+#                                 print('audit log:',
+#                                       user.groups[choice].bind_hosts[
+#                                           user_option].audit_logs)
+#                                 ssh_login.ssh_login(user,
+#                                                     user.groups[
+#                                                         choice].bind_hosts[
+#                                                         user_option],
+#                                                     session,
+#                                                     log_recording)
+#                 else:
+#                     print("no this option..")
